071724a09790679dd228bd26ff5a88bbd53ccebe
take1412 <taketake1412@yahoo.co.jp>
2020-12-08 18:23:41 +0900

    単語モデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "wordlists#index"
-<<<<<<< Updated upstream
-  resources :wordlists, only: [:index, :new, :create, :show]
-=======
   resources :wordlists do
     resources :words only: [:new, ]
   end
->>>>>>> Stashed changes
 end
