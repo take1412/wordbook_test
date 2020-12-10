2d049efad753fd0c38d6c612881381df9899e409
take1412 <taketake1412@yahoo.co.jp>
2020-12-11 01:04:52 +0900

    見た目編集

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,8 +2,8 @@ class WordsController < ApplicationController
 
   def index
     @wordlist = Wordlist.find(params[:wordlist_id])
-    @words = @wordlist.words.includes(:user)
-    @words = @wordlist.words.page(params[:page]).per(10)
+    # @words = @wordlist.words.includes(:user)
+    @words = @wordlist.words.page(params[:page]).per(1)
   end
 
   def new
