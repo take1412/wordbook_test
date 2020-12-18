2343646f891980fccc2e16f8a17c0da93d4b7e96
take1412 <taketake1412@yahoo.co.jp>
2020-12-18 15:44:50 +0900

    ユーザー詳細画面実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    user = User.find(params[:id])
-    if current_user.id == user.id
-      @nickname = user.nickname
-      @wordlists = user.wordlists.page(params[:page]).per(10).order('created_at DESC')
-    else
-      redirect_to root_path
-    end
+    @user = User.find(params[:id])
+    @nickname = @user.nickname
+    @wordlists = @user.wordlists.page(params[:page]).per(10).order('created_at DESC')
   end
 end
