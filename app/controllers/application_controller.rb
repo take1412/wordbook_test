fe2a21d6740bb911e5e1ab3c83b382fb6c65d389
take1412 <taketake1412@yahoo.co.jp>
2021-02-03 22:25:20 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :user_code])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :user_code])
   end
 end
